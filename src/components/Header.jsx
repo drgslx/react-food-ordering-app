c2e8604a13cc60268472b104e556bc2f0c449442
Dragos-Stefan Alexa <drgs_lx@yahoo.com>
2024-07-05 14:53:49 +0300

    created the modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Button from '../components/UI/Button'
-import logoImg from '../assets/logo.jpg'
+import { useContext } from 'react';
+
+import Button from './UI/Button.jsx';
+import logoImg from '../assets/logo.jpg';
+import CartContext from '../store/CartContext.jsx';
+
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
-    <header id='main-header'>
-        <div id="title">
-            <img src={logoImg} alt="A restauran" />
-            <h1>ReactMeals</h1>
-        </div>
-            <nav>
-                <Button textOnly={true}>Cart</Button>
-            </nav>
-            
-        
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart ({totalCartItems})</Button>
+      </nav>
     </header>
-  )
+  );
 }
